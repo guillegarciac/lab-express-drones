0beb6deb80d38e31894c19fcdf1429d12b30197d
guille garcia <ggarciacardiel@gmail.com>
2023-01-11 20:04:35 +0100

    it2 - added route to get list of drones + created partial to diplay tags for each drone

--- routes/drones.js
+++ routes/drones.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model');
 
-router.get('/drones', (req, res, next) => {
+/* GET all drones */
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  try {
+    const drones = await Drone.find({});
+    res.render("drones/list", { drones });
+  } catch (error) {
+    next(error);
+  }
 });
+
+router.get ("/", async (req,res,next) => {
+  
+})
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
