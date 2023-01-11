bfe0b4a6e6284c13cf04f02d4daf0a2c6f7f6dd2
guille garcia <ggarciacardiel@gmail.com>
2023-01-11 19:39:50 +0100

    it1 - created seed, model and exported to dB

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,22 @@
-// Iteration #1
+const mongoose = require ("mongoose");
+const Drone = require ("../models/Drone.model");
+const MONGODB_URI = 'mongodb://localhost:27017/lab-express-drones';
+mongoose.set('strictQuery', true);
+const drones = require('../data/drones');
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    return Drone.deleteMany();
+  })
+  .then(() => {
+    return Drone.create(drones);
+  })
+  .then(createdDrones => console.log(createdDrones))
+  .then(() => {
+    mongoose.disconnect();
+  })
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  });
